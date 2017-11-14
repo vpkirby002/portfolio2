7d77d8fd3c3837b4bb6990bf2530e49a873734c3
vpkirby002 <vpkirby002@gmail.com>
2017-11-13 23:18:51 -0500

    calendar updates

--- Intent-Valerie2/app/assets/javascripts/full_calendar.js
+++ Intent-Valerie2/app/assets/javascripts/full_calendar.js
@@ -6,6 +6,7 @@ initialize_calendar = function() {
 		var calendar = $(this);
 		calendar.fullCalendar({
 			header: {
+				left: 'prev,next today',
 				center: 'title',
 				right: 'month,agendaWeek,agendaDay'
 			},
@@ -13,18 +14,34 @@ initialize_calendar = function() {
 			selectHelper: true,
 			editable: true,
 			eventLimit: true,
-			
+			events: '/events.json',
 			select: function(start, end) {
 				$.getScript('/events/new', function(){
 					$('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"));
-					date_range_picker();
+					daterangepicker();
 					$('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
 					$('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
 
 				});
 				
 				calendar.fullCalendar('unselect'); 
+			},
+			eventDrop: function(event, delta, revertFunc) {
+				event_data = {
+					event: {
+						id: event.id,
+						start: event.start.format(),
+						end: event.end.format()
+					}
+				};
+				$.ajax({
+					url: event.update_url,
+					data: event_data,
+					type: 'PATCH'
+				});
 			}
+
+
 		});
 	})
 };
