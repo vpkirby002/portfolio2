e0a1aa14803454fbf090e04686e6fc56c0495f81
Valerie Kirby <31776413+vpkirby002@users.noreply.github.com>
2018-02-25 15:28:51 -0500

    real portfolio2 Feb. 25, 2018

---
+++ portfolio2/main.js
@@ -0,0 +1,12 @@
+
+
+// Code taken from codepen for hamburger menu below
+$(function() {
+  $('.js-nav a, .js-connect').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
+});
+
